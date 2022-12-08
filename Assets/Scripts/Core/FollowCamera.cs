0afae1daba2b1823fa3b29802ab970b548194706
Aaro Paukkunen <aaro.paukkunen@gmail.com>
2022-12-08 17:25:15 +0200

    Basic movement and sandbox

---
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField] Transform target;
+
+        // LateUpdate prevents camera jittering
+        // in this instance, because with just update
+        // it tries to wrestle itself to position simultaneously
+        // as the animator updates (update === once per frame)
+        // --> see unity docs for order of exec! 
+        void LateUpdate()
+        {
+            transform.position = target.position;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Combat;
+using RPG.Core;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField] Transform target;
+
+        NavMeshAgent navMeshAgent;
+
+        private void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            GetComponent<Fighter>().Cancel();
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Stop()
+        {
+            navMeshAgent.isStopped = true;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMeshAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+        }
+    }
+
+}
